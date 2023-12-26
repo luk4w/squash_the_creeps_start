7ba0ba47f279f1d91fdd96f62316bc4e71e5a259
Luk4w <luk4w@outlook.com>
2023-12-26 00:46:32 -0300

    player created

---
+++ scripts/Player.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    [Export]
+    public int Speed { get; set; } = 14;
+
+    [Export]
+    public int FallAcceleration { get; set; } = 75;
+
+    private Vector3 _targetVelocity = Vector3.Zero;
+
+    // Função que trabalha com física, não atualiza o mais rapido possivel, trabalha com um valor fixo de atualizações por segundo
+    public override void _PhysicsProcess(double delta)
+    {
+        // Input direction
+        var direction = Vector3.Zero;
+
+        if (Input.IsActionPressed("move_right"))
+        {
+            direction.X += 1.0f;
+        }
+
+        if (Input.IsActionPressed("move_left"))
+        {
+            direction.X -= 1.0f;
+        }
+
+        if (Input.IsActionPressed("move_back"))
+        {
+            direction.Z += 1.0f;
+        }
+
+        if (Input.IsActionPressed("move_forward"))
+        {
+            direction.Z -= 1.0f;
+        }
+
+        // Evita o movimento mais rapido do player na diagonal (modulo do vetor resultante da direção maior que 1)
+        if (direction != Vector3.Zero)
+        {
+            direction = direction.Normalized();
+            GetNode<Node3D>("Pivot").LookAt(Position + direction, Vector3.Up);
+        }
+
+        _targetVelocity.X = direction.X * Speed;
+        _targetVelocity.Z = direction.Z * Speed;
+
+        // Vertical velocity
+        if (!IsOnFloor()) // Se estiver no ar, aplicar um "efeito" de gravidade
+        {
+            _targetVelocity.Y -= FallAcceleration * (float)delta;
+        }
+
+        // Mover o player
+        Velocity = _targetVelocity;
+        // Move o personagem suavemente (Se atingir uma parede no meio de um movimento, a engine tentará suavizar essa ação)
+        MoveAndSlide();
+
+    }
+
+}
